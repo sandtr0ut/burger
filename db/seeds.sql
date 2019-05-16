d776d0ad106320b4e1be8d3ccfa3e965cea9e115
sandtr0ut <crosbyverse@gmail.com>
2019-05-16 16:37:03 -0700

    complete mysql code

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Wagyu Sliders', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Mushroom Swiss Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('RF Big-Ass Burger', false);
