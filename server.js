fe604671b72f5a01b57889e0adc62aae23eb0a4e
sandtr0ut <crosbyverse@gmail.com>
2019-05-16 23:34:17 -0700

    code express and exphbs to server

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+var exphbs = require("express-handlebars");
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Import Routes and Let the Server Use Them
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 app.listen(PORT, () => console.log(`App listensing on PORT ${PORT}...`));
