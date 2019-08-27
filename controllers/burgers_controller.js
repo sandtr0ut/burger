2cbf08658e71c2b8e39d9e9471c8054cf2b8a668
sandtr0ut <crosbyverse@gmail.com>
2019-08-26 22:16:17 -0700

    repair model and controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,59 +1,41 @@
-const express = require("express");
+var express = require('express');
 
-const router = express.Router();
+var router = express.Router();
+var burger = require('../models/burger.js');
 
-// Import Model
-const burger = require("../models/burger.js")
-
-//========================================
-//ROUTES
-//========================================
-
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-        var hbsObject = {
-            burger: data
-        };
-        console.log(hbsObject);
-    });
+// get route -> index
+router.get('/', function(req, res) {
+  res.redirect('/burgers');
 });
 
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function(result) {
-        res.json({ id: result.insertId })
-    });
+router.get('/burgers', function(req, res) {
+  // express callback response by calling burger.selectAllBurger
+  burger.all(function(burgerData) {
+    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+    res.render('index', { burger_data: burgerData });
+  });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    
-    console.log("condition", condition);
-    
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function(result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+// post route -> back to index
+router.post('/burgers/create', function(req, res) {
+  // takes the request object using it as input for burger.addBurger
+  burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect('/');
+  });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    burger.delete(condition, function(result) {
-        if (result.affectedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+// put route -> back to index
+router.put('/burgers/:id', function(req, res) {
+  burger.update(req.params.id, function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    // Send back response and let page reload from .then in Ajax
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
-
--- models/burger.js
+++ models/burger.js
@@ -1,30 +1,25 @@
-const orm = require("../config/orm.js");
+var orm = require('../config/orm.js');
 
 var burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-        });
-    },
-    
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-            cb(res)
-        }); 
-    },
-    
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-    
-    delete: function(condition, cb) {
-        orm.delete("burgers", condition, function(res) {
-            cd(res);
-        });
-    }
+  all: function(cb) {
+    orm.all('burgers', function(res) {
+      cb(res);
+    });
+  },
+  create: function(name, cb) {
+    orm.create('burgers', ['burger_name', 'devoured'], [name, false], cb);
+  },
+  update: function(id, cb) {
+    var condition = 'id=' + id;
+    orm.update(
+      'burgers',
+      {
+        devoured: true
+      },
+      condition,
+      cb
+    );
+  }
 };
 
 module.exports = burger;
-
